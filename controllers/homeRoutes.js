ccd195d90a3f589059b2327dbe85733f7211f164
Tyson Jackson <tyson@alittlerdesigner>
2022-11-28 22:56:46 +0800

    test

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -35,7 +35,7 @@ router.get('/profile', withAuth, async (req, res) => {
 });
 
 
-router.get('/login', (req, res) => {
+router.get('/', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect('/profile');
